32f0ec7d654d6b5d36e7711d9a9cdfd97467005b
TatevEloyan <tatev_eloyan@mail.ru>
2020-09-18 18:21:03 +0400

    linked list implementation

--- homeworks/collections/src/main/java/CustomLinkedList.java
+++ homeworks/collections/src/main/java/CustomLinkedList.java
@@ -1,4 +1,39 @@
 package main;
 
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
 public class CustomLinkedList {
+    Node head;
+
+    public void add(int data){
+        Node temp;
+        if(head == null) {
+            head = new Node(data);
+            return;
+        }
+        temp = head;
+        while (temp.next != null)
+            temp = head.next;
+        temp.next = new Node(data);
+    }
+    public void print(){
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+    public static void main(String []arg){
+        CustomLinkedList list = new CustomLinkedList();
+        list.add(4);
+        list.add(7);
+        list.add(1);
+        list.print();
+    }
 }
