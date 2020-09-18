ab8cf75a02cf30d8bc0c2fafb5606885fef33f1c
TatevEloyan <tatev_eloyan@mail.ru>
2020-09-18 22:22:53 +0400

    linked list implementation

--- homeworks/collections/src/main/java/CustomLinkedList.java
+++ homeworks/collections/src/main/java/CustomLinkedList.java
@@ -9,31 +9,60 @@ class Node{
     }
 }
 public class CustomLinkedList {
-    Node head;
+    private Node head;
+    private int size;
+
+    public CustomLinkedList(){
+        size = 0;
+    }
 
     public void add(int data){
-        Node temp;
+        Node temp = head;
         if(head == null) {
             head = new Node(data);
+            size++;
             return;
         }
-        temp = head;
+
         while (temp.next != null)
-            temp = head.next;
+            temp = temp.next;
         temp.next = new Node(data);
+        size++;
     }
-    public void print(){
+    public void remove(int data){
+        Node temp = head;
+        while (temp.next.data != data){
+            temp = temp.next;
+            if(temp.next == null)
+                return;
+        }
+        temp.next = temp.next.next;
+        size--;
+    }
+    public int getSize(){
+        return size;
+    }
+
+    @Override
+    public String toString(){
         Node temp = head;
         while (temp != null){
             System.out.println(temp.data);
             temp = temp.next;
         }
+        return "";
     }
+
     public static void main(String []arg){
         CustomLinkedList list = new CustomLinkedList();
         list.add(4);
         list.add(7);
         list.add(1);
-        list.print();
+        list.add(8);
+        list.add(9);
+        list.add(6);
+        list.remove(7);
+        System.out.println(list);
+        System.out.println("List Size is " + list.getSize());
     }
 }
