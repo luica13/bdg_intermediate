75b13032f9d42ebe4c6cf482169cfd44c4245c4b
GeghamKesoyan <geghamkesoyan@gmail.com>
2020-09-23 19:33:04 +0400

    Test1

--- homeworks/collections/src/main/java/CustomLinkedList.java
+++ homeworks/collections/src/main/java/CustomLinkedList.java
@@ -1,4 +1,43 @@
 package main;
 
 public class CustomLinkedList {
+
+}public class LinkedListImplementation {
+    public class Node {
+        int data;
+        Node next;
+    }
+    public Node head;
+    public Node tail;
+    public int size;
+    public int size(){
+        return this.size;
+
+    }
+
+    public int getFirst() {
+        return this.head.data;
+    }
+    public int getLast(){
+        return this.tail.data;
+
+    }
+    public int getAt(int idx){
+        Node temp = this.head;
+        for (int i = 1; i <= idx; i++){
+            temp = temp.next;
+
+        }
+        return temp.data;
+    }
+    Node getNodeAt(int idx){
+        Node temp = this.head;
+        for (int i = 1; i <= idx; i++){
+            temp = temp.next;
+
+        }
+        return temp;
+
+    }
+
 }
