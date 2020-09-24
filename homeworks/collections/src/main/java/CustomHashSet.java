33d0447ee9bc986a85034e30c53e579c6614345b
Vardan Soghomonyan <vardan.soghomonyan@sfl.am>
2020-09-24 16:03:36 +0400

    HashSet implementation modified to generic

--- homeworks/collections/src/main/java/CustomHashSet.java
+++ homeworks/collections/src/main/java/CustomHashSet.java
@@ -3,32 +3,33 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class CustomHashSet {
-    private Set<Integer> hashSet;
+public class CustomHashSet<T> {
+    private Set<T> hashSet;
 
-    public Set<Integer> getHashSet() {
+    public Set<T> getHashSet() {
         return hashSet;
     }
 
     //initialise new HashSet
-    void initHashSet(Integer... intArray) {
-        hashSet = new HashSet<>(Arrays.asList(intArray));
+    @SafeVarargs
+    final void initHashSet(T... array) {
+        hashSet = new HashSet<>(Arrays.asList(array));
         System.out.println("New HashSet is initialised with " + hashSet + " values");
     }
 
     //add new item
-    boolean addItem(int a) {
+    boolean addItem(T a) {
         return hashSet.add(a);
     }
 
     //remove new item
-    boolean removeItem(int a) {
+    boolean removeItem(T a) {
         return hashSet.remove(a);
     }
 
     //remove all items
     void removeAllItems() {
-        Iterator<Integer> iterator = hashSet.iterator();
+        Iterator<T> iterator = hashSet.iterator();
         while (iterator.hasNext()) {
             System.out.println(iterator.next() + " will be removed.");
             iterator.remove();
--- homeworks/collections/src/main/java/Main.java
+++ homeworks/collections/src/main/java/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        CustomHashSet customHashSet = new CustomHashSet();
+        CustomHashSet<Integer> customHashSet = new CustomHashSet<>();
         customHashSet.initHashSet(5, 6, 1, 9, 4);
         if (customHashSet.addItem(4)) {
-            System.out.println("Number is added and new HashSet is: " + customHashSet.getHashSet());
+            System.out.println("New item is added and new HashSet is: " + customHashSet.getHashSet());
         }
         if (customHashSet.removeItem(5)) {
-            System.out.println("Number is removed and new HashSet is: " + customHashSet.getHashSet());
+            System.out.println("An item is removed and new HashSet is: " + customHashSet.getHashSet());
         }
         customHashSet.removeAllItems();
     }
