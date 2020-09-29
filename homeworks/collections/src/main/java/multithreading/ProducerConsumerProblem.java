f1c8cd527642a804db183ec519a5efd734bab481
TatevEloyan <tatev_eloyan@mail.ru>
2020-09-29 23:27:16 +0400

    homeworks/collections/src/main/java/multithreading/ProducerConsumerProblem.java

---
+++ homeworks/collections/src/main/java/multithreading/ProducerConsumerProblem.java
@@ -0,0 +1,13 @@
+package multithreading;
+
+public class ProducerConsumerProblem {
+
+    public static void main(String[] args) {
+        Producer producer = new Producer();
+        producer.setName("producer");
+        producer.start();
+        Consumer consumer = new Consumer(producer);
+        consumer.setName("consumer");
+        consumer.start();
+    }
+}
