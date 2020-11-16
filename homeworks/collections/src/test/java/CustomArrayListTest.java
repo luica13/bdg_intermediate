57ba360c3c85faba8fdcd330083e4d23a998f393
TatevEloyan <tatev_eloyan@mail.ru>
2020-11-16 23:18:39 +0400

    unit test cover

--- homeworks/collections/src/test/java/CustomArrayListTest.java
+++ homeworks/collections/src/test/java/CustomArrayListTest.java
@@ -1,14 +1,12 @@
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
 public class CustomArrayListTest {
 
     private static CustomArrayList customArrayList;
     private int size;
 
-    @BeforeClass
-    public static void setup(){
+    @Before
+    public  void setup(){
         customArrayList = new CustomArrayList();
     }
 
@@ -28,9 +26,52 @@ public class CustomArrayListTest {
 
     @Test
     public void sizeShouldDecreaseWhenRemoveElement(){
+        customArrayList.add(5);
+        customArrayList.add(4);
         customArrayList.remove(1);
         size = customArrayList.getSize();
         Assert.assertEquals(1, size);
     }
 
+    @Test
+    public void sizeShouldIncreaseWhenAddElementUsingIndex(){
+        customArrayList.add(0, 77);
+        size = customArrayList.getSize();
+        Assert.assertEquals(1, size);
+    }
+
+    @Test
+    public void getItemMethodShouldReturnRequiredItem(){
+        customArrayList.add(1);
+        customArrayList.add(2);
+        Object i = customArrayList.getItem(0);
+        Object i2 = customArrayList.getItem(1);
+        Assert.assertEquals(1, i);
+        Assert.assertEquals(2, i2);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void shouldGiveExceptionWhenIndexLesserThen0(){
+        customArrayList.add(-1, 4);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void shouldGiveExceptionWhenIndexGreaterThenSize(){
+        customArrayList.add(0, 4);
+        customArrayList.add(3,7);
+    }
+
+    @Test
+    public void shouldUpdateCapacityWhenSizeLessThenDefaultCapacity(){
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+        int capacity = customArrayList.getCapacity();
+        Assert.assertEquals(16, capacity);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldGiveExceptionWhenCapacityLesserThen0(){
+        CustomArrayList customArrayList = new CustomArrayList(-4);
+    }
 }
