0e5273ff0885522b9096fa2917a2ec45648a3010
luica13 <luiza.kharatyan@gmail.com>
2020-11-24 01:20:14 +0400

    initial corrections

--- ObserverPattern/src/Main.java
+++ ObserverPattern/src/Main.java
@@ -1,18 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        Channel channel = new Channel();
 
+        Channel channel = new Channel();
         ArmenianObserver armenian = new ArmenianObserver("Սամվել");
         AmericanObserver american = new AmericanObserver("Jack");
         RussianObserver russian = new RussianObserver("Павел");
 
         armenian.subscribe(channel);
         american.subscribe(channel);
+        russian.subscribe(channel);
 
-        System.out.println("First video uploaded");
+        System.out.println("\nFirst video uploaded");
+        sleep(400L);
         channel.uploadVideo("Java Programming");
-        System.out.println("Second video uploaded");
-        channel.uploadVideo("Observer Pattern");
 
+        sleep(1000L);
+        System.out.println("\nSecond video uploaded");
+        sleep(400L);
+        channel.uploadVideo("Observer Pattern");
+        sleep(1000L);
+
+    }
+
+    private static void sleep(long milliseconds ) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
