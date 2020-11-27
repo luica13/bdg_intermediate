ca2746e5ee228d8550d4f202da9d25d4cade68cd
KhachikSaghatelyan <xsaghatelyan@mail.ru>
2020-11-28 00:06:46 +0400

    Consumer

---
+++ src/main/java/Homework/producer_consumer1/Consumer.java
@@ -0,0 +1,48 @@
+package Homework.producer_consumer1;
+
+import java.util.ArrayList;
+
+public class Consumer implements Runnable {
+
+    private ArrayList<Integer> q;
+
+    public Consumer(ArrayList<Integer> q) {
+        this.q = q;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Inside run method of Consumer thread.");
+        while(true){
+            synchronized (q) {
+                if(q.size() != 0){
+                    for(int i=0; i<10; i++){
+                        q.remove(0);
+                    }
+                    System.out.println("Queue is empty, sending notification to Procucer");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+// TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    q.notify();
+
+                }
+                else{
+                    try {
+                        q.wait();
+                    } catch (InterruptedException e) {
+// TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+
+
+    }
+
+}
+
