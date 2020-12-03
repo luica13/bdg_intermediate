117651545925e0984dd094e91116a4676126227d
David <davidkhachatrian@gmail.com>
2020-12-03 20:32:54 +0400

    Implement wait and notify method

---
+++ homeworks/producer_consumer/src/producerConsumer/Consumer.java
@@ -0,0 +1,37 @@
+package producerConsumer;
+
+import java.util.List;
+
+public class Consumer implements Runnable {
+    List<Integer> questionList = null;
+
+    public Consumer(List<Integer> questionList) {
+        this.questionList = questionList;
+    }
+
+    public void answerQuestion() throws InterruptedException {
+        synchronized (questionList) {
+            while (questionList.isEmpty()) {
+                System.out.println("No Question to answer... Waiting for producer to get questions");
+                questionList.wait();
+            }
+        }
+        synchronized (questionList) {
+            Thread.sleep(5000);
+            System.out.println("Answered question: " + questionList.remove(0));
+            questionList.notify();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                answerQuestion();
+            } catch (InterruptedException e) {
+                throw new IllegalArgumentException("Thread is interrupted!");
+            }
+        }
+    }
+}
+    
