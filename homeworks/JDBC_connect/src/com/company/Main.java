be5f430fa600512ee3e34da215777c3e9c362e98
Eleonorharutyunyan <70915229+Eleonorharutyunyan@users.noreply.github.com>
2020-12-11 23:39:30 +0400

    Update Main.java

--- homeworks/JDBC_connect/src/com/company/Main.java
+++ homeworks/JDBC_connect/src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+//        AuthorList authorList = new AuthorList();
+//
+//        authorList.authorFill();
+//        authorList.authorPrint();
+        Ambulance general = new Ambulance();
+//        AuthorWindow authorWindow = new AuthorWindow();
+
     }
 }
