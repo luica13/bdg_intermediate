f39067493bb478591e4941301b9d17b7fe1112f8
Hrachya Khachatryan <hro070890@gmail.com>
2020-12-21 11:18:26 +0400

    created main class

---
+++ bank_transaction/src/main/java/am/bdg/intermediate_group_2_W_S/bank_transaction/am/bdg/intermediate_group_2_W_S/BankTransactionApplication.java
@@ -0,0 +1,12 @@
+package am.bdg.intermediate_group_2_W_S.bank_transaction.am.bdg.intermediate_group_2_W_S;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankTransactionApplication.class, args);
+    }
+}
