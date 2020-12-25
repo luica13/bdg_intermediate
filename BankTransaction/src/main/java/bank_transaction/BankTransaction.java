f466ab90b3960bea6f59532565eea37440bd7fe8
Arman Grigoryan <grigoryanarmankn@gmail.com>
2020-12-25 21:49:27 +0400

    Bank Transaction Implementation

---
+++ BankTransaction/src/main/java/bank_transaction/BankTransaction.java
@@ -0,0 +1,11 @@
+package bank_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransaction {
+    public static void main(String[] args) {
+        SpringApplication.run(BankTransaction.class, args);
+    }
+}
